e979493d747329b830c4dadb5f2d7936ec7edac7
pulidu <godagepulindu@gmail.com>
2026-02-21 14:22:22 +0530

    12th commit created & updated custom exception file

---
+++ src/main/java/com/example/sprin_pos_system/exception/CustomException.java
@@ -0,0 +1,7 @@
+package com.example.sprin_pos_system.exception;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
