ad224cc219af4e96d710e2036700a0f1e90f369d
pulidu <godagepulindu@gmail.com>
2026-02-21 14:21:31 +0530

    11th commit created & updated springpossystemapplication java file & tests.java files

---
+++ src/main/java/com/example/sprin_pos_system/SprinPosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprin_pos_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinPosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinPosSystemApplication.class, args);
+	}
+
+}
